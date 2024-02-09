de8a4127d15f30722cd84bb611210a0f479ffef6
Nikitin De <456_1996@mail.ru>
2024-02-10 00:37:06 +0300

    result

--- src/task/io/ReadWriteFileExceptionHandling.java
+++ src/task/io/ReadWriteFileExceptionHandling.java
@@ -5,32 +5,27 @@ import java.util.Scanner;
 
 public class ReadWriteFileExceptionHandling {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число: ");
-        int number1 = scanner.nextInt();
-        System.out.println("Введите второе число: ");
-        int number2 = scanner.nextInt();
-        scanner.nextLine();
-
-        int res = dividesNumbers(number1, number2);
+        int res = dividesNumbers();
         System.out.println("Результат = " + res);
-        System.out.println();
-
-        System.out.println("Введите слово: ");
-        String str = scanner.nextLine();
-        int result = stringConversion(str);
+        int result = stringConversion();
         System.out.println("Результат: "+ result);
 
     }
     // TODO: Cоздайте метод, который делит два числа и возвращает результат. Обработайте исключение,
     //    //  если второе число равно нулю
-    public static int  dividesNumbers(int number1, int number2) {
+    public static int  dividesNumbers() {
+
         try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите первое число: ");
+            int number1 = scanner.nextInt();
+            System.out.println("Введите второе число: ");
+            int number2 = scanner.nextInt();
             if (number2 == 0) {
                 throw new RuntimeException();
             }
            return number1 / number2;
-        } catch (Exception e) {
+        } catch (ArithmeticException e) {
             System.out.println("На ноль делить нельзя");
 
         }
@@ -40,12 +35,15 @@ public class ReadWriteFileExceptionHandling {
 
     // TODO: Создайте метод, который преобразует строку в число. Обработайте исключение, если строка не
     //  может быть преобразована в число.
-    public static int  stringConversion(String str) {
+    public static int  stringConversion() {
         try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите слово: ");
+            String str = scanner.nextLine();
             return Integer.parseInt(str);
-        } catch (Exception ex) {
-            System.out.println("Строка не преобразована:" + str);
-            return 0;
+        } catch (NumberFormatException ex) {
+            System.out.println("Строка не преобразована:" );
+        return 0;
         }
     }
 
